64cde6d612bb4fd827f11777f63464ebe35a0be2
Charles Odili <chaluwa@gmail.com>
2023-03-16 22:17:04 -0600

    refactor for code reuse in flask app

--- src/meme.py
+++ src/meme.py
@@ -1,69 +1,25 @@
 """Turbo meme generator - core entrypoint"""
-
-import os
 import random
-
-import filetype
 from argparse import ArgumentParser
 
+from utilities import Utils
 from memeengine import MemeEngine
-from quoteengine import SmartIngestor, Quote
 
 _IMAGES_DIR = "./_data/photos/dog/"
 _QUOTES_DIR = "./_data/DogQuotes/"
-
-def get_quote_for_meme(body, author, quotes_dir):
-    quote = None
-
-    if body is None:
-        quotes = []
-        quote_files = []
-        for root, dirs, files in os.walk(quotes_dir):
-            quote_files = [os.path.join(root, name) for name in files]
-
-        for file in quote_files:
-            quotes.extend(SmartIngestor.parse(file))
-
-        quote = random.choice(quotes)
-    else:
-        if author is None:
-            raise ValueError('author is required when body is provided')
-        quote = Quote(body, author)
-
-    return quote
-
-def get_img_for_meme(path, images_dir):
-    img = None
-    images = filetype.image_matchers
-    if path is None:
-        # images_dir will be "./_data/photos/dog/" or
-        # a directory of custom images specified by the user
-        imgs = []
-        for root, dirs, files in os.walk(images_dir):
-            # pull all the files
-            imgs = [os.path.join(root, name) for name in files]
-            # only the image files
-            imgs = [file for file in imgs if filetype.match(file, matchers=images) is not None]
-
-        img = random.choice(imgs)
-    else:
-        if filetype.match(path, matchers=images) is not None:
-            img = path
-        else:
-            raise ValueError("path does not appear to be a valid image file")
-    
-    return img
 
 def generate_meme(path=None, body=None, author=None, images=_IMAGES_DIR, quotes=_QUOTES_DIR):
     """ Generate a meme given an path and a quote """
 
     images_dir = _IMAGES_DIR if images is None else images
-    img = get_img_for_meme(path, images_dir)
+    imgs = Utils.get_imgs_for_meme(images_dir, path)
+    img = random.choice(imgs)
 
     quotes_dir = _QUOTES_DIR if quotes is None else quotes
-    quote = get_quote_for_meme(body, author, quotes_dir)
+    qts = Utils.get_quotes_for_meme(quotes_dir, body, author)
+    quote = random.choice(qts)
 
-    meme = MemeEngine('./generated/memes')
+    meme = MemeEngine('./static')
     generated_img = meme.generate(img, quote.body, quote.author)
     return generated_img
 
---
+++ src/utilities/utils.py
@@ -0,0 +1,46 @@
+import os
+
+import filetype
+
+from quoteengine import SmartIngestor, Quote
+
+class Utils():
+
+    def get_quotes_for_meme(quotes_dir, body=None, author=None):
+        quotes = []
+
+        if body is None:
+            
+            quote_files = []
+            for root, dirs, files in os.walk(quotes_dir):
+                quote_files = [os.path.join(root, name) for name in files]
+
+            for file in quote_files:
+                quotes.extend(SmartIngestor.parse(file))
+
+        else:
+            if author is None:
+                raise ValueError('author is required when body is provided')
+            quotes = [Quote(body, author)]
+
+        return quotes
+
+    def get_imgs_for_meme(images_dir, path=None):
+        imgs = []
+        images = filetype.image_matchers
+        if path is None:
+            # images_dir will be "./_data/photos/dog/" or
+            # a directory of custom images specified by the user
+            for root, dirs, files in os.walk(images_dir):
+                # pull all the files
+                imgs = [os.path.join(root, name) for name in files]
+                # only the image files
+                imgs = [file for file in imgs if filetype.match(file, matchers=images) is not None]
+
+        else:
+            if filetype.match(path, matchers=images) is not None:
+                imgs = [path]
+            else:
+                raise ValueError("path does not appear to be a valid image file")
+        
+        return imgs
